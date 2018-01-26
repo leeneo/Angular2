ccfddc9a2f9482a94a55c412f31bae5f80345bec
leeneo <leeneo@xingzhihen.com>
2018-01-26 09:08:53 +0800

    1

--- Todo/src/app/app.module.ts
+++ Todo/src/app/app.module.ts
@@ -1,17 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // [(ngModel)]双向绑定语法，依赖于FormsModule组件；
+
 
 import { AppComponent } from './app.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
