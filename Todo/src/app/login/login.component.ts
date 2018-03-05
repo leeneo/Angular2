806542df9fcd40b0955ce47ff4624914a1e4c2d3
leeneo <leeneo@xingzhihen.com>
2018-03-05 15:59:10 +0800

    1

--- Todo/src/app/login/login.component.ts
+++ Todo/src/app/login/login.component.ts
@@ -3,34 +3,8 @@ import { Component, OnInit, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  template: `
-    <div class='loginWrap'>
-      <form #formRef='ngForm' (ngSubmit)='onSubclck(formRef.value)'>
-      <fieldset ngModelGroup='login'>
-          <div *ngIf='usernameRef.errors?.required'>must be required</div>
-          <div *ngIf='usernameRef.errors?.minlength'>minlenght>3</div>
-          <p>
-            <span>用户名：</span>
-            <input name='username' required minlength='3' [(ngModel)]='username' #usernameRef='ngModel' placeholder='请输入用户名' type='text'>
-          </p>
-          <div *ngIf='passwordRef.errors?.required'>must be required</div>
-          <p>
-            <span>密　码：</span>
-            <input name='password' required [(ngModel)]='password' #passwordRef='ngModel' type='password' placeholder='请输入密码'>
-          </p>
-          <button type='submit' class='loginBtn'>Login</button>
-        </fieldset>
-      </form>
-    </div>
-  `,
-  styles: [`
-    .loginWrap{width:274px;margin:0 auto;overflow:hidden;text-align:center;}
-    input{display: inline-block}
-    span{width:20%}
-    .loginBtn{float:right}
-    input.ng-invalid{outline:1px solid red}
-    input.ng-valid{outline:1px solid gold}
-  `],
+  template: './login.component.html',
+  styles: [`./login.component.css`],
   // DI注入方式，直接引用&本地提供；或者通过令牌方式，在构造中引用
   // providers: [AuthService]
 })
--- Todo/src/main.ts
+++ Todo/src/main.ts
@@ -1,10 +1,12 @@
-import { enableProdMode } from '@angular/core';
+import './polyfills.ts';
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
+import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
+import { AppModule } from './app/app.module';
 
-enableProdMode();
+if (environment.production) {
+  enableProdMode();
+}
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+platformBrowserDynamic().bootstrapModule(AppModule);
